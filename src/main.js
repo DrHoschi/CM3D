27caa4df38bfde8917aed2d1838194f7c88be0c4
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-01 15:35:21 +0200

    WD-20D.1: expose dependency graph test build

--- src/main.js
+++ src/main.js
@@ -128,9 +128,9 @@ const projectSettings = installProjectSettings(store, appUI);
 const cameraObjectPreview = installCameraObjectPreview(store, runtime, appUI);
 const inspectorDiagnostics = installInspectorDiagnostics(store, runtime, appUI);
 
-document.title = 'CyberMotion 3D – WD-20C.4';
+document.title = 'CyberMotion 3D – WD-20D.1';
 const buildLabel = document.querySelector('.brand small');
-if (buildLabel) buildLabel.textContent = 'WD-20C.4';
+if (buildLabel) buildLabel.textContent = 'WD-20D.1';
 
 const focusButton=document.querySelector('#focus-selection');
 const syncFocusButton=()=>{if(focusButton)focusButton.disabled=!store.getObject(store.selection.activeObjectId);};
