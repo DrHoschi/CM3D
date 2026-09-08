743d61576fd03b6ac8667d38cb0d606e26509ae0
DrHoschi <mann_alexander2000@yahoo.de>
2026-09-08 21:50:04 +0200

    WD-21C.4-R1: expose correction build identity

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ import { installProjectSettings } from './ui/project-settings.js';
 import { installCameraObjectPreview } from './ui/camera-object-preview.js';
 import { installInspectorDiagnostics } from './ui/inspector-diagnostics.js';
 
-const BUILD_ID = 'WD-21C.4';
+const BUILD_ID = 'WD-21C.4-R1';
 const applyBuildIdentity = () => {
   document.title = `CyberMotion 3D – ${BUILD_ID}`;
   const buildLabel = document.querySelector('.brand small');
